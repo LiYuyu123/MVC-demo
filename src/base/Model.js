7ab97e50b12b4697a3132e7053af98bc28646c8a
LiYu <2248868140@qq.com>
2021-07-26 18:07:32 +0800

    完成了eventBus继承

--- src/base/Model.js
+++ src/base/Model.js
@@ -1,6 +1,10 @@
-class Model{
+import EventBus from "./eventBus.js";
+
+class Model extends EventBus{
     constructor(options) {
-        ['data','create','delete','update','get'].forEach((key)=>{
+        super()
+        const keys= ['data','create','delete','update','get']
+        keys.forEach((key)=>{
             if(key in options){
                 this[key]=options[key]
             }
